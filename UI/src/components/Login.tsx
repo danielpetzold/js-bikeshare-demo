21dc5265f9e79740270903eb0517cf6b1f659954
Chad Lumley <chad.lumley@projekt202.com>
2019-02-14 12:02:48 -0800

    CHAD - Cleaning up grid system

--- UI/src/components/Login.tsx
+++ UI/src/components/Login.tsx
@@ -3,8 +3,10 @@ import './Login.scss';
 
 const Login = () => (
   <div className={'grid'}>
-    <div className={'col-width-4'}>
-      <h1>Hello, World!</h1>
+    <div className={'grid__row'}>
+      <div className={'grid__column-4 grid__column-m-2'}>
+        <h1>Hello, World!</h1>
+      </div>
     </div>
   </div>
 );
