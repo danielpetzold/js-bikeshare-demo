9d1fe5a9cf863668c1cb89a6485fb91194eed7e2
Logan Mace <loganmace.dev@gmail.com>
2019-02-22 09:56:20 -0600

    Built the responsive layout for the login page and added _buttons scss file for reusable styles

--- UI/src/components/Login/Login.tsx
+++ UI/src/components/Login/Login.tsx
@@ -1,14 +1,91 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Login.scss';
+import logo from '../../assets/group-41.svg';
 
-const Login = () => (
-  <div className={'grid'}>
-    <div className={'grid__row'}>
-      <div className={'grid__column-4 grid__column-m-2'}>
-        <h1>Hello, World!</h1>
+interface State {
+  selectedRole: string;
+}
+
+export default class Login extends Component<State> {
+  state = {
+    selectedRole: ''
+  };
+
+  selectRole = (e: React.MouseEvent<HTMLDivElement>) => {
+    if (!(e.target instanceof HTMLDivElement)) {
+      return;
+    }
+    this.setState({
+      selectedRole: e.target.id
+    });
+  };
+
+  handleLogin = () => {};
+
+  render() {
+    const { selectedRole } = this.state;
+    return (
+      <div className={'grid login'}>
+        <div className={'grid__row'}>
+          <div className={'grid__column-4 grid__column-m-4'}>
+            <div className={'login__wrapper'}>
+              <img src={logo} alt="logo" className={'login__logo'} />
+              <div className={'login__content'}>
+                <div className={'login__welcome'}>
+                  <h2 className={'login__welcome'}>Welcome back</h2>
+                  <p className={'login__welcome'}>Sign in to continue</p>
+                </div>
+                <div className={'login__options'}>
+                  <div
+                    className={
+                      'login__options__role' +
+                      (selectedRole === 'COO'
+                        ? ' login__options__role--active'
+                        : '')
+                    }
+                    id="COO"
+                    onClick={this.selectRole}
+                  >
+                    COO
+                  </div>
+                  <div
+                    className={
+                      'login__options__role login__options__role--mid' +
+                      (selectedRole === 'Regional'
+                        ? ' login__options__role--active'
+                        : '')
+                    }
+                    id="Regional"
+                    onClick={this.selectRole}
+                  >
+                    Regional
+                  </div>
+                  <div
+                    className={
+                      'login__options__role' +
+                      (selectedRole === 'Local'
+                        ? ' login__options__role--active'
+                        : '')
+                    }
+                    id="Local"
+                    onClick={this.selectRole}
+                  >
+                    Local
+                  </div>
+                </div>
+                <button
+                  className={'login__button btn--primary--green'}
+                  disabled={!selectedRole}
+                  onClick={this.handleLogin}
+                >
+                  Login
+                </button>
+              </div>
+              <div />
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-);
-
-export default Login;
+    );
+  }
+}
