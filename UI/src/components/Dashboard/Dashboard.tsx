05404f5a721a31da86e9d309bb79875d154a6076
Chad Lumley <chad.lumley@projekt202.com>
2019-02-25 11:03:47 -0800

    CHAD - Responsive styling for header filter links

--- UI/src/components/Dashboard/Dashboard.tsx
+++ UI/src/components/Dashboard/Dashboard.tsx
@@ -3,6 +3,10 @@ import './Dashboard.scss';
 import NavBar from '../NavBar/NavBar';
 
 class Dashboard extends React.Component<any, any> {
+  openFilter() {
+    console.log('test');
+  }
+
   render() {
     return (
       <div>
@@ -12,9 +16,27 @@ class Dashboard extends React.Component<any, any> {
             <div className={'grid dashboard__header'}>
               <div className={'grid__row'}>
                 <div
-                  className={'grid__column-3 grid__column-m-4 dashboard__title'}
+                  className={'grid__column- grid__column-m-4 dashboard__title'}
                 >
                   Trends and Analytics
+                </div>
+              </div>
+              <div className={'grid__row'}>
+                <div className={'grid__column-12 grid__column-m-4'}>
+                  <div
+                    className={'dashboard__region-filter'}
+                    onClick={this.openFilter}
+                  >
+                    {`All Regions`}{' '}
+                    <i className="icon-ic-arrow-down dashboard__down-arrow-icon" />
+                  </div>
+                  <div
+                    className={'dashboard__region-time-frame'}
+                    onClick={this.openFilter}
+                  >
+                    {`Last Quarter`}{' '}
+                    <i className="icon-ic-unfold-more dashboard__unfold-icon" />
+                  </div>
                 </div>
               </div>
             </div>
