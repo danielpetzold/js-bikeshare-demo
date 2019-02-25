34495cc5362215b0801deb1e424331a7b87fd4e7
Logan Mace <loganmace.dev@gmail.com>
2019-02-25 11:49:53 -0600

    Built temporary structure for no access overlay and added route to view while building

---
+++ UI/src/components/NoAccess/NoAccess.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function NoAccess() {
+  return (
+    <div>
+      <img src="" alt="no-access icon" />
+      <h1>What just happened?</h1>
+      <p>
+        Sorry, but that’s all you can see so far with the demo software. If you
+        want to see more, you could always purchase Jaspersoft.
+      </p>
+      <button>Contact Sales Team</button>
+    </div>
+  );
+}
