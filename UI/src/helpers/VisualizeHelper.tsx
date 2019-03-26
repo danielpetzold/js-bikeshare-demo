272af3b517436c44361af0a9b0ee270ce464902d
Chad Lumley <chad.lumley@projekt202.com>
2019-03-26 12:33:56 -0700

    CHAD - Adding themeHref support to visualize helper

--- UI/src/helpers/VisualizeHelper.tsx
+++ UI/src/helpers/VisualizeHelper.tsx
@@ -1,4 +1,3 @@
-import * as constants from './constants';
 /**
  * Helper functions for interfacing with Visualizer.js global instance.
  */
@@ -13,6 +12,7 @@ class VisualizeHelper {
    * Type definitions do not yet exist for Visualize.js. Casting as any, for now.
    */
   private viz = (window as any).visualize;
+  private theme = (window as any).themeHref;
 
   /**
    * Token-Based Authentication Login
@@ -20,9 +20,29 @@ class VisualizeHelper {
    * @param jaspersoftServerUrl
    */
   login(userToken: string, jaspersoftServerUrl: string) {
+
+    if (typeof this.theme !== 'undefined') {
+      // JRS runs on a URL like http[s]://my.server.com[:8080]/jasperserver-pro
+      // The JRS URL points to http[s]://another.server.com[:9999]/jasperserver-pro
+      // or a relative /jasperserver-pro
+      var jrsPathToFind = "/jasperserver-pro";
+      var posOfRelative = this.theme.indexOf(jrsPathToFind);
+      if (posOfRelative > 0) {
+        this.theme = '/jasperserver-pro' + this.theme.substring(posOfRelative + jrsPathToFind.length);
+      }
+    } else {
+      // app served visualize.js script
+      // point to themes installed in web app
+      this.theme = "css/jasper/default";
+      // if you want to use the JRS served themes, remove theme part of the object below.
+    }
+
     this.viz.config({
       server: jaspersoftServerUrl,
       scripts: 'optimized-scripts',
+      // theme: {
+      //   href: this.theme
+      // },
       auth: {
         loginFn: (properties: any, request: any) => {
           return request({
@@ -71,6 +91,25 @@ class VisualizeHelper {
         });
       });
     });
+  }
+
+  getAdHocView(uiContainer: string, resourcePath: string, params: any = {}) {
+    return new Promise((resolve, reject) => {
+      this.viz((v: any) => {
+        v.adhocView({
+          container: `#${uiContainer}`,
+          resource: resourcePath,
+          params: params,
+          success: (success: any) => {
+            resolve(success);
+          },
+          error: (err: any) => {
+            console.log('getAdHocView', err);
+            reject(err);
+          }
+        });
+      })
+    })
   }
 
   /**
