468c0a94c60fd4fa818c81864acebc7cf88f9333
Chad Lumley <chad.lumley@projekt202.com>
2019-04-01 10:08:58 -0700

    CHAD - Working on refactoring layout

--- UI/src/pages/Dashboard/Dashboard.tsx
+++ UI/src/pages/Dashboard/Dashboard.tsx
@@ -139,63 +139,125 @@ class Dashboard extends React.Component<DashboardProps, State> {
             data={this.filters}
           />
         ) : null }
-        <div className={'main'}>
-          <div className={'main__content'}>
+        <div className={'dashboard'}>
 
-            <div className={'grid dashboard__header '}>
+          <header className={'page-header'}>
+            <div className={'page-header__content grid'}>
               <div className={'grid__row'}>
                 <div className={'grid__column-4 grid__column-m-4'}>
-                  <div className='dashboard__title'> Trends and Analytics</div>
-                  <div className={'dashboard__region-filter'} onClick={() => this.setState({ isFilterOpen: true })}>
+                  <div className='page-header__title'> Trends and Analytics</div>
+                  <div className={'page-header__region-filter'} onClick={() => this.setState({ isFilterOpen: true })}>
                     {this.state.selectedFilters['Region'] ? this.state.selectedFilters['Region'].label : "Please select Region"}
-                    <i className="icon-ic-arrow-down dashboard__down-arrow-icon" />
+                    <i className="icon-ic-arrow-down page-header__down-arrow-icon" />
                   </div>
                 </div>
-                <div className={'grid__column-4 grid__column-m-4'}></div>
+                <div className={'grid__column-4 page-header__spacer'}></div>
                 <div className={'grid__column-4 grid__column-m-4'}>
-                  <div className={'dashboard__region-time-frame'} onClick={() => this.setState({ isFilterOpen: true })}>
-                      {this.state.selectedFilters['Timeframe'] ? this.state.selectedFilters['Timeframe'].label : "Please select Timeframe"}
-                      <i className="icon-ic-unfold-more dashboard__unfold-icon" />
+                  <div className={'page-header__region-time-frame'} onClick={() => this.setState({ isFilterOpen: true })}>
+                    {this.state.selectedFilters['Timeframe'] ? this.state.selectedFilters['Timeframe'].label : "Please select Timeframe"}
+                    <i className="icon-ic-unfold-more page-header__unfold-icon" />
                   </div>
                 </div>
               </div>
             </div>
+          </header>
+
+          <div className={'dashboard__body'}>
 
             <div className={'dashboard__map'}>
-              <div className={'dashboard__map-toggle'}>
+              <div className={'dashboard__map-controls'}>
+                <div className={'dashboard__toggle-map'}>
+
+                </div>
               </div>
-              <div className={'dashboard__map-container'}>
-              </div>
+              <div className={'dashboard__placeholder-map'}></div>
             </div>
 
-            <div className={'grid dashboard__body'}>
+            <div className={'dashboard__centered grid'}>
               <div className={'grid__row dashboard__KPI'}>
                 <div className={'grid__column-12 grid__column-m-4'}>
                   <div id={'kpi-report'} />
                 </div>
               </div>
-              <div className={'grid__row'}>
-                <div className={'grid__column-8 grid__column-m-4'}>
-                  <div id={'in-need-report'} />
-                </div>
-                <div className={'grid__column-4 grid__column-m-4'}>
-                  <div id={'active-times-report'} />
-                </div>
+            </div>
+            <div className={'grid__row'}>
+              <div className={'grid__column-8 grid__column-m-4'}>
+                <div id={'in-need-report'} />
               </div>
-              <div className={'grid__row'}>
-                <div className={'grid__column-4 grid__column-m-4'}>
-                  <div id={'active-times-report'} />
-                </div>
-                <div className={'grid__column-4 grid__column-m-4'}>
-                  <div id={'active-times-report'} />
-                </div>
-                <div className={'grid__column-4 grid__column-m-4'}>
-                  <div id={'active-times-report'} />
-                </div>
+              <div className={'grid__column-4 grid__column-m-4'}>
+                <div id={'active-times-report'} />
+              </div>
+            </div>
+            <div className={'grid__row'}>
+              <div className={'grid__column-4 grid__column-m-4'}>
+                <div id={'active-times-report'} />
+              </div>
+              <div className={'grid__column-4 grid__column-m-4'}>
+                <div id={'active-times-report'} />
+              </div>
+              <div className={'grid__column-4 grid__column-m-4'}>
+                <div id={'active-times-report'} />
               </div>
             </div>
           </div>
+
         </div>
+        {/*<div className={'main'}>*/}
+          {/*<div className={'main__content'}>*/}
+            {/*<div className={'grid dashboard__header '}>*/}
+              {/*<div className={'grid__row'}>*/}
+                {/*<div className={'grid__column-4 grid__column-m-4'}>*/}
+                  {/*<div className='dashboard__title'> Trends and Analytics</div>*/}
+                  {/*<div className={'dashboard__region-filter'} onClick={() => this.setState({ isFilterOpen: true })}>*/}
+                    {/*{this.state.selectedFilters['Region'] ? this.state.selectedFilters['Region'].label : "Please select Region"}*/}
+                    {/*<i className="icon-ic-arrow-down dashboard__down-arrow-icon" />*/}
+                  {/*</div>*/}
+                {/*</div>*/}
+                {/*<div className={'grid__column-4 grid__column-m-4'}></div>*/}
+                {/*<div className={'grid__column-4 grid__column-m-4'}>*/}
+                  {/*<div className={'dashboard__region-time-frame'} onClick={() => this.setState({ isFilterOpen: true })}>*/}
+                      {/*{this.state.selectedFilters['Timeframe'] ? this.state.selectedFilters['Timeframe'].label : "Please select Timeframe"}*/}
+                      {/*<i className="icon-ic-unfold-more dashboard__unfold-icon" />*/}
+                  {/*</div>*/}
+                {/*</div>*/}
+              {/*</div>*/}
+            {/*</div>*/}
+
+            {/*<div className={'dashboard__map'}>*/}
+              {/*<div className={'dashboard__map-toggle'}>*/}
+              {/*</div>*/}
+              {/*<div className={'dashboard__map-container'}>*/}
+              {/*</div>*/}
+            {/*</div>*/}
+
+            {/*<div className={'grid dashboard__body'}>*/}
+              {/*<div className={'grid__row dashboard__KPI'}>*/}
+                {/*<div className={'grid__column-12 grid__column-m-4'}>*/}
+                  {/*<div id={'kpi-report'} />*/}
+                {/*</div>*/}
+              {/*</div>*/}
+              {/*<div className={'grid__row'}>*/}
+                {/*<div className={'grid__column-8 grid__column-m-4'}>*/}
+                  {/*<div id={'in-need-report'} />*/}
+                {/*</div>*/}
+                {/*<div className={'grid__column-4 grid__column-m-4'}>*/}
+                  {/*<div id={'active-times-report'} />*/}
+                {/*</div>*/}
+              {/*</div>*/}
+              {/*<div className={'grid__row'}>*/}
+                {/*<div className={'grid__column-4 grid__column-m-4'}>*/}
+                  {/*<div id={'active-times-report'} />*/}
+                {/*</div>*/}
+                {/*<div className={'grid__column-4 grid__column-m-4'}>*/}
+                  {/*<div id={'active-times-report'} />*/}
+                {/*</div>*/}
+                {/*<div className={'grid__column-4 grid__column-m-4'}>*/}
+                  {/*<div id={'active-times-report'} />*/}
+                {/*</div>*/}
+              {/*</div>*/}
+            {/*</div>*/}
+          {/*</div>*/}
+        {/*</div>*/}
       </>
     );
   }
