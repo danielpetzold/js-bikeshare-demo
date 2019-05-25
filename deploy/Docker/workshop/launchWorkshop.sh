d9e03afdf67249debad340b00c4cd5ff4438c6a3
Sherman Wood <sgwood63@users.noreply.github.com>
2019-05-25 12:51:22 -0700

    Workshop launch and scripts
    Manage Docker launch/stop of
    - Portainer
    - Pgadmin 4
    - Workshop docker-compose
    Jaspersoft Studio

---
+++ deploy/Docker/workshop/launchWorkshop.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+docker start portainer
+
+docker start pgadmin4
+
+cd /home/ubuntu/Documents/js-bikeshare-demo/deploy/Docker/development
+docker-compose up -d
+
+cd ~/Documents
+
+"./TIB_js-jss_7.2.1/Jaspersoft Studio Professional" > /dev/null 2>&1 &
+
+
---
+++ deploy/Docker/workshop/stopWorkshop.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+docker stop portainer
+
+docker stop pgadmin4
+
+cd /home/ubuntu/Documents/js-bikeshare-demo/deploy/Docker/development
+docker-compose down
+
+cd ~/Documents
+
+#"./TIB_js-jss_7.2.1/Jaspersoft Studio Professional" &
+
+
