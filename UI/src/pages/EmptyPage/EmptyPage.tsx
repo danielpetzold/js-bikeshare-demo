f7649e73729d5e177004f3a30da918913d70c8c6
Sherman Wood <sgwood63@users.noreply.github.com>
2019-06-05 14:41:39 -0700

    Real Bikeshare theme
    A properly exported theme

--- UI/src/pages/EmptyPage/EmptyPage.tsx
+++ UI/src/pages/EmptyPage/EmptyPage.tsx
@@ -38,32 +38,30 @@ class EmptyPage extends Component<DashboardProps, EmptyPageState> {
   changeDetailsReport = (e: any, link: any) => {
     e.preventDefault();
     visualizeHelper.getReport(dashboard_report_uri_root + '/' + link.href, 'detail-report', this.getParams())
-        .then((success: any) => {
-		var report: any = success.report;
-		var result: any = success.success;
-		this.setState({ detailReport: report });
-        });
+    .then((success: any) => {
+				var report: any = success.report;
+				var result: any = success.success;
+				this.setState({ detailReport: report });
+    });
   }
 
   async componentDidMount() {
-	/*
       visualizeHelper.getReport(kpi_report_uri, 'kpi-report', this.getParams(), {
         events: {
           click: this.changeDetailsReport
         }
-      })
-        .then((success: any) => {
-		var report: any = success.report;
-		var result: any = success.success;
-		this.setState({ kpiReport: report });
+       })
+       .then((success: any) => {
+					var report: any = success.report;
+					var result: any = success.success;
+					this.setState({ kpiReport: report });
         });
       visualizeHelper.getReport(default_detail_report_uri, 'detail-report', this.getParams())
         .then((success: any) => {
-		var report: any = success.report;
-		var result: any = success.success;
-		this.setState({ detailReport: report });
-        });
-    */
+				var report: any = success.report;
+				var result: any = success.success;
+				this.setState({ detailReport: report });
+      });
   }
 
 
@@ -79,7 +77,29 @@ class EmptyPage extends Component<DashboardProps, EmptyPageState> {
     return params;
   };
 
+  exportAReport = (e: any) => {
+    e.preventDefault();
+
+   let report: any = this.state.detailReport;
+
+    report.export({
+      //export options here        
+      outputFormat: 'pdf'
+      //exports all pages if not specified
+      //pages: "1-2"
+		}, function (link: any) {
+			 var url = link.href ? link.href : link;
+			 window.location.href = url;
+		}, function (error: any) {
+				console.log(error);
+		});
+  }
+
   render() {
+    const {
+      detailReport
+    } = this.state;
+
 
     return (
       <>
@@ -89,17 +109,17 @@ class EmptyPage extends Component<DashboardProps, EmptyPageState> {
             <div className={'grid__row jspage-header'}>
               <div className={'grid__column-12 grid__column-m-4'}>
                 <div className={'jspage-header__top'}>
-                  <h3 className={'jspage-header__title'}>I am empty!</h3>
+                  <h3 className={'jspage-header__title'}>I am alive!!!!!</h3>
                 </div>
 
                 {/* Bottom Header Row */}
                 <div className={'jspage-header__bottom'}>
                   <div className={'jspage-header__buttons'}>
-                    <button className={'jspage-view__btn--create btn--primary'}>
-                      Create
+                    <button className={'jspage-view__btn--create btn--primary'} onClick={this.exportAReport} >
+                      Export Detail
                     </button>
                     <button className={'jspage-view__btn--actions btn--secondary'}>
-                      Modify / Export
+                      Button 2
                     </button>
                   </div>
                 </div>
